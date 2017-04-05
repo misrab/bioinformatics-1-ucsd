e2b9c97eb60de204450270afc09f28e03befe17c
Misrab <faizullah.misrab@gmail.com>
2017-04-05 17:23:00 +0800

    autocommit

--- week3/motifs/src/main.rs
+++ week3/motifs/src/main.rs
@@ -10,25 +10,72 @@
 //     remove duplicates from Patterns
 //     return Patterns
 
-use std::io;
+// use std::io;
 use std::io::BufReader;
 use std::io::BufRead;
 use std::fs::File;
-use std::path::Path;
+// use std::path::Path;
+use std::collections::HashSet;
+use std::env;
 
-fn MotifEnumeration(dna: &[std::string::String], k: usize, d: u8) {
+
+pub fn pattern_match(d: u8, template: &str, pattern: &str) -> bool {
+    let k = pattern.len();
+    let mut subtemplate;
+    for i in 0..(template.len() - k + 1) {
+        subtemplate = &template[i..i+k];
+        let mut mismatches = 0;
+        for j in 0..k {
+            if subtemplate.as_bytes()[j] != pattern.as_bytes()[j] {
+                mismatches += 1;
+            }
+        }
+        if mismatches <= d {
+            return true;
+        }
+    }
+
+    false
+}
+
+
+fn motif_enumeration(dna: &[std::string::String], k: usize, d: u8) {
     println!("Running Motif Enumeration...");
+
+    let mut matches = HashSet::new();
+    let mut tried = HashSet::new();
 
     for line in dna {
         // go over k-mers in line
-        for i in 0..(line.len() - k + 1) {
+        'kmer: for i in 0..(line.len() - k + 1) {
             let kmer = &line[i..i+k];
-            println!("{:?}", kmer);
+
+            // if we've tried this kmer just continue
+            if tried.contains(kmer) { continue 'kmer; }
+            else { tried.insert(kmer); }
+
+            // println!("{:?}", tried);
+
+            // now do a d-pattern match across each dna string
+            // if not found in any one, it's disqualified
+            'dna: for line2 in dna {
+                let qualified = pattern_match(d, line2, kmer);
+                if !qualified {
+                    // println!("Found valid kmer {:?}", kmer);
+                    // try next kmer
+                    continue 'kmer;
+                }
+            }
+            // if we get here it means it's in all dna strings
+            matches.insert(kmer);
+
         }
     }
+
+    println!("{:?}", matches);
 }
 
-fn ReadFile(filename: &str) -> Vec<String> {
+fn read_file(filename: &str) -> Vec<String> {
     let f = File::open(filename).unwrap();
     let buf = BufReader::new(&f);
 
@@ -39,12 +86,34 @@ fn ReadFile(filename: &str) -> Vec<String> {
 fn main() {
     println!("Hello, world!");
 
-    let lines = ReadFile("./src/sample.txt");
+    let args: Vec<_> = env::args().collect();
+    println!("Args provided: {:?}", args);
+
+    let lines = read_file("./src/sample.txt");
 
     let line1: Vec<&str> = lines[0].split(" ").collect();
     let (k, d) = (line1[0].parse::<usize>().unwrap(), line1[1].parse::<u8>().unwrap());
     let dna = &lines[1..];
 
-    MotifEnumeration(dna, k, d);
+    motif_enumeration(dna, k, d);
 
+}
+
+
+//
+// Testing
+//
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn pattern_matching() {
+        let mut matched = pattern_match(3, &"abcd".to_string(), &"efgh".to_string());
+        assert!(matched==false);
+
+        matched = pattern_match(3, &"abcd".to_string(), &"bcc".to_string());
+        assert!(matched==true);
+    }
 }
